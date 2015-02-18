ec71974660f2d876e458a3b879772c99c1089c29
Leire L S <parecementeria@gmail.com>
2015-02-18 19:38:24 +0000

    bit of style

--- js/components/contactInfo.react.js
+++ js/components/contactInfo.react.js
@@ -10,14 +10,23 @@ var ContactInfo = React.createClass({
 
     return (
       <div className="col-md-9 jumbotron">
+        <div className="btn-group pull-right">
+          <button 
+            type="button" 
+            className="btn btn-danger" 
+            aria-label="Left Align"
+            onClick={this._onClick.bind(null, this.props.contact.id)} >
+              <span className="glyphicon glyphicon-trash" aria-hidden="true"></span>
+          </button>
+          <button 
+            type="button" 
+            className="btn btn-info" 
+            aria-label="Center Align">
+              <span className="glyphicon glyphicon-edit" aria-hidden="true"></span>
+          </button>
+        </div>
         <h2>{this.props.contact.name}</h2>
         <p>{this.props.contact.info}</p>
-        <button 
-          type="button" 
-          className="btn btn-danger" 
-          onClick={this._onClick.bind(null, this.props.contact.id)} >
-            Delete contact
-        </button>
       </div>
     );
   },
